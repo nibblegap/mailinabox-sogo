1218e8775278ad2f9433cc3dded4110cfa01d11f
Brent Dacus <22330436+bdacus01@users.noreply.github.com>
2021-11-28 15:51:56 -0600

    no socket in mysql config file

--- setup/mysql.sh
+++ setup/mysql.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-source setup/functions.sh # load our functions
+source setup/functions.sh   # load our functions
 source /etc/mailinabox.conf # load global vars
 
 echo "Installing MariaDB server..."
 
 apt_install mariadb-server
-
+#Enable at boot
+systemctl enable mariadb >>/dev/null
 # We need this password for later to auth Dovecot, Postfix and SOGo.
 # TODO: This isn't optimal. We should do better... later...
-MIAB_SQL_PW=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13`
+MIAB_SQL_PW=$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)
 
 # Mailinabox database name
 MIAB_SQL_DB="mailinabox"
@@ -20,11 +21,11 @@ if [ ! -d $MYSQL_DATADIR ]; then
     # Move the datadir to $STORAGE_ROOT/mail/mysql/ for backup reasons
 
     # Stop the MySQL daemon as we're editing the config file
-    service mariadb stop >> /dev/null
+    service mariadb stop >>/dev/null
 
     # Change the datadir location for MySQL
     tools/editconf.py /etc/mysql/mariadb.conf.d/50-server.cnf datadir=$MYSQL_DATADIR
-
+    tools/editconf.py /etc/mysql/mariadb.conf.d/50-server.cnf socket=/run/mysqld/mysqld.sock
     # Create the new database location in our $STORAGE_ROOT
     mkdir -p $MYSQL_DATADIR
 
@@ -38,22 +39,22 @@ if [ ! -d $MYSQL_DATADIR ]; then
     mkdir -p /var/lib/mysql/mysql
 
     # Help apparmor detect the new MySQL home and then restart apparmor
-    echo "alias /var/lib/mysql/ -> $MYSQL_DATADIR," >> /etc/apparmor.d/tunables/alias
+    echo "alias /var/lib/mysql/ -> $MYSQL_DATADIR," >>/etc/apparmor.d/tunables/alias
     restart_service apparmor
 
     # Restart the MySQL daemon
-    restart_service mysql
+    restart_service mariadb
 
     # Create the MIAB and SOGo database
-    mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE ${MIAB_SQL_DB}; CREATE DATABASE ${SOGO_SQL_DB};" >> /dev/null
+    mysql --defaults-file=/etc/mysql/debian.cnf -e "CREATE DATABASE ${MIAB_SQL_DB}; CREATE DATABASE ${SOGO_SQL_DB};" >>/dev/null
 
     # Import our preconfigured database to MySQL
-    mysql --defaults-file=/etc/mysql/debian.cnf ${MIAB_SQL_DB} < conf/mailinabox_init.sql >> /dev/null
+    mysql --defaults-file=/etc/mysql/debian.cnf ${MIAB_SQL_DB} <conf/mailinabox_init.sql >>/dev/null
 fi
 
 # Grant privs for mailinabox user so that postfix/dovecot and SOGo can interact with the DB
 # Note that the password changes on every install/upgrade
-mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON ${MIAB_SQL_DB}.* TO 'mailinabox'@'%' IDENTIFIED BY '${MIAB_SQL_PW}'; GRANT ALL PRIVILEGES ON ${SOGO_SQL_DB}.* TO 'mailinabox'@'%' IDENTIFIED BY '${MIAB_SQL_PW}'; FLUSH PRIVILEGES;" >> /dev/null
+mysql --defaults-file=/etc/mysql/debian.cnf -e "GRANT ALL PRIVILEGES ON ${MIAB_SQL_DB}.* TO 'mailinabox'@'%' IDENTIFIED BY '${MIAB_SQL_PW}'; GRANT ALL PRIVILEGES ON ${SOGO_SQL_DB}.* TO 'mailinabox'@'%' IDENTIFIED BY '${MIAB_SQL_PW}'; FLUSH PRIVILEGES;" >>/dev/null
 
 ### TESTING
 # Set root password to 1234, and allow connections from anywhere for root user
